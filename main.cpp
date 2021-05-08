ff770abc79a35b8232bfc4eb2c33483b98868494
Luis Angel Angel <>
2021-05-08 23:39:28 +0000

    agregué comentarios

--- main.cpp
+++ main.cpp
@@ -29,43 +29,56 @@ int main() {
   cout << "ingrese un numero entre 0 y 999\n";
   cin >> numero;
 
-    /*  descomposición de variable "numero" en: unidades, decenas y centenas. */
+        /*  Verifico que el número esté en el rango apropiados  */
+  if(numero < 0 ||  numero  > 999 ){
+    cout  <<  "¡El número indicado está fuera de rango!\n"
+              "ejecute el programa de nuevo si desea reintentarlo.";
+    return 0;
+  }
+
+    /*  descomposición de variable "numero" en: unidades, decenas y centenas.
+        que se usarán como los índices (o subíndices) de los vectores apropiados
+        para presentar el número en letras; sean cientos, decenas o unidades
+    */
   
-  indunidades = numero%10;
-  inddecenas  = numero/10%10;
-  indcentenas = numero/100%10;
+  indunidades = numero%10;    //indunidades representa el "índice las unidades"
+  inddecenas  = numero/10%10; //inddecenas representa el "índice las decenas"
+  indcentenas = numero/100%10;//indcentenas representa el "índice las centenas"
 
   //  cout  <<  indunidades << endl;  //  Se usará como índice del vector "unidades"
   //  cout  <<  inddecenas << endl;   //  Se usará como índice del vector "decenas"
   //  cout  <<  indcentenas << endl;  //  Se usará como índice del vector "centenas"
 
   if(numero < 20){
-    cout  <<  unidades[numero];   //  Despliegue si el número es de los primeros veinte
+    cout  <<  unidades[numero];       //Despliegue si el número es de los primeros veinte y me salgo
     return 0;
   }
 
   if(numero ==  100){
-    cout  <<  "cien";             //  La excepción del número 100
+    cout  <<  "cien";                 //Presentar la excepción del número 100 y me salgo
     return 0;
   }
 
-  if(indcentenas  > 0 ){
+  if(indcentenas  > 0 ){              //Si hay centenas, continuo con el despliegue de las decenas
     cout  <<  centenas[indcentenas];
-    if(inddecenas > 1 ){
-      cout  <<  " " <<  decenas[--inddecenas];
+    if(inddecenas > 1 ){              //Si las decenas son mayores a "20" las despliego como: "veinte", "treinta"..
+      cout  <<  " " <<  decenas[--inddecenas];      //Presento las decenas si están por encima de 20
     }
     else{
-      cout  <<  " " <<  unidades[inddecenas*10+indunidades];
+      cout  <<  " " <<  unidades[inddecenas*10+indunidades];  //De no ser así, presento el número hasta "dicecinueve"
       return 0;
     }    
     if(inddecenas ==  0 ){
       if(indunidades  ==  0){
         return 0;
       }
-      cout  <<  " " <<  unidades[indunidades];
+      cout  <<  " " <<  unidades[indunidades];       // De nuevo, puro formato: solo espacio en blanco + unidades
       return 0;
     }
-    cout  <<  " y " <<  unidades[indunidades];
+    else{
+      cout  <<  " y " <<  unidades[indunidades];       // De nuevo, puro formato: agrego las unidades a las decenas
+      return 0;
+    }    
     return 0;
   }
 
